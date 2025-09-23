02cb717aa3a8a3a48fbf1ea2942b4c07331a7adc
kaz <kaz>
2025-09-23 19:49:33 -0400

    more

--- lix/lix.py
+++ lix/lix.py
@@ -224,7 +224,5 @@ def parse_file_lid_v5(p):
 
 
 
-if __name__ == '__main__':
-    path = '/home/kaz/Downloads/2508703_LAB_20250820_161106.lid'
-    parse_file_lid_v5(path)
+
     
---
+++ main.py
@@ -0,0 +1,6 @@
+from lix.lix import parse_file_lid_v5
+
+
+if __name__ == '__main__':
+    path = '/home/kaz/Downloads/2508703_LAB_20250820_161106.lid'
+    parse_file_lid_v5(path)

12ed9caaf31fb6067bc6b2fb1c64a8e5a8f4a6f2
kaz <kaz>
2025-09-23 15:27:13 -0400

    ore

--- lix/lix.py
+++ lix/lix.py
@@ -1,35 +1,16 @@
 import os
 import subprocess as sp
-from functools import lru_cache
-
-from mat.ascii85 import ascii85_to_num
-from mat.temperature import Temperature
+import sys
+from ascii85 import ascii85_to_num
+from temperature import LixFileConverterT
 
 
 
 # file size, sample length, min. mask length, chunk size
-SL = 18
 MML = 1
 CS = 256
 MASK_TIME_EXTENDED = 0x40
-
-
-
-class LixFileConverterT:
-    def __init__(self, a, b, c, d, r):
-        self.coefficients = dict()
-        self.coefficients['TMA'] = a
-        self.coefficients['TMB'] = b
-        self.coefficients['TMC'] = c
-        self.coefficients['TMD'] = d
-        self.coefficients['TMR'] = r
-        self.cnv = Temperature(self)
-
-    @lru_cache
-    def convert(self, raw_temperature):
-        # _p(f'\nLixFileConverterT coefficients {self.coefficients}')
-        # _p(f'raw T {raw_temperature} converted T {self.cnv.convert(raw_temperature)}')
-        return self.cnv.convert(raw_temperature)
+g_glt = ''
 
 
 
@@ -72,6 +53,12 @@ def _decode_sensor_measurement(s, x):
 
 
 
+def _parse_macro_header(bb):
+    global g_glt
+    g_glt = ''
+    g_glt = bb[:3].decode()
+    print('glt', g_glt)
+
 
 
 def _parse_mini_header(bb):
@@ -99,29 +86,40 @@ def _parse_mask(bb):
 
 
 def _parse_sample(bb, t, fo):
-    # CTD bytes 2P, 2T, 6A, 8C
-    bb_t = bb[0:2]
-    temp = _decode_sensor_measurement('T', bb_t)
-    temp_as_celsius = '{:06.3f}'.format(float(lct.convert(temp)))
-    bb_p = bb[2:4]
-    pres = _decode_sensor_measurement('P', bb_p)
-    bb_a = bb[4:10]
-    ac_x = _decode_sensor_measurement('A', bb_a[0:2])
-    ac_y = _decode_sensor_measurement('A', bb_a[2:4])
-    ac_z = _decode_sensor_measurement('A', bb_a[4:6])
-    bb_c = bb[10:]
-    c0 = int.from_bytes(bb_c[0:2], byteorder='big', signed=False)
-    c1 = int.from_bytes(bb_c[2:4], byteorder='big', signed=False)
-    c2 = int.from_bytes(bb_c[4:6], byteorder='big', signed=False)
-    c3 = int.from_bytes(bb_c[6:8], byteorder='big', signed=False)
-    print('c2 = 0x{:02x}'.format(c1))
-    fo.write(f'{t},{temp},{temp_as_celsius},{pres},{ac_x},{ac_y},{ac_z},{c0},{c1},{c2},{c3}\n')
+
+    temp = ''
+    temp_as_celsius = ''
+    pres = ''
+    ac_x = ''
+    ac_y = ''
+    ac_z = ''
+
+    if g_glt in ('TDO', 'CTD'):
+        bb_t = bb[0:2]
+        temp = _decode_sensor_measurement('T', bb_t)
+        temp_as_celsius = '{:06.3f}'.format(float(lct.convert(temp)))
+        bb_p = bb[2:4]
+        pres = _decode_sensor_measurement('P', bb_p)
+        bb_a = bb[4:10]
+        ac_x = _decode_sensor_measurement('A', bb_a[0:2])
+        ac_y = _decode_sensor_measurement('A', bb_a[2:4])
+        ac_z = _decode_sensor_measurement('A', bb_a[4:6])
+
+    if g_glt == 'TDO':
+        fo.write(f'{t},{temp},{temp_as_celsius},{pres},{ac_x},{ac_y},{ac_z}\n')
+
+    if g_glt == 'CTD':
+        bb_c = bb[10:]
+        c0 = int.from_bytes(bb_c[0:2], byteorder='big', signed=False)
+        c1 = int.from_bytes(bb_c[2:4], byteorder='big', signed=False)
+        c2 = int.from_bytes(bb_c[4:6], byteorder='big', signed=False)
+        c3 = int.from_bytes(bb_c[6:8], byteorder='big', signed=False)
+        fo.write(f'{t},{temp},{temp_as_celsius},{pres},{ac_x},{ac_y},{ac_z},{c0},{c1},{c2},{c3}\n')
 
 
 
 
-
-def parse_file_lidxv5(p):
+def parse_file_lid_v5(p):
     i = 0
     need_parse_mini = 1
 
@@ -141,20 +139,45 @@ def parse_file_lidxv5(p):
 
     # separate macro_header
     bb_macro_header = bb[:CS]
-    bb = bb[CS:-n_pad]
-    data_size = len(bb)
+    _parse_macro_header(bb_macro_header)
 
-    path_csv = p.replace('.lid', '_CTD.csv')
+
+    # get variables depending on logger type
+    sl = 0
+    csv_column_titles = ''
+    suffix = ''
+    if g_glt == 'TDO':
+        sl = 10
+        csv_column_titles = f't,temp,temp(c),pres,Ax,Ay,Az\n'
+        suffix = 'TDO'
+    elif g_glt == 'CTD':
+        sl = 18
+        csv_column_titles = f't,temp,temp(c),pres,Ax,Ay,Az,c0,c1,c2,c3\n'
+        suffix = 'CTD'
+    elif g_glt.startswith('DO'):
+        csv_column_titles = f'dotheseones'
+        sl = 6
+        suffix = 'DissolvedOxygen'
+    else:
+        sys.exit(1)
+
+
+    # start CSV file
+    path_csv = p.replace('.lid', f'_{suffix}.csv')
     print(path_csv)
-
     print(f'output csv file = {path_csv}')
     f_csv = open(path_csv, 'w')
-    f_csv.write(f't,temp,temp(c),pres,Ax,Ay,Az,c0,c1,c2,c3\n')
+    f_csv.write(csv_column_titles)
+
+
+    # separate rest of file
+    bb = bb[CS:-n_pad]
+    data_size = len(bb)
 
 
     # parse it measurement by measurement
     while 1:
-        if i + SL + MML > data_size:
+        if i + sl + MML > data_size:
             # real or padded
             print(f'end at {i}, data_size {data_size}, remain {data_size - i}')
             break
@@ -168,28 +191,33 @@ def parse_file_lidxv5(p):
             m = (i // CS) * CS
             _parse_mini_header(bb[m:m+8])
 
-        n_mask, t = _parse_mask(bb[i:i+1])
 
-        if (i % CS) + n_mask + SL > CS:
+        # mask first
+        n_mask, t = _parse_mask(bb[i:i+2])
+
+        if (i % CS) + n_mask + sl > CS:
             n_pre = CS - (i % CS)
-            n_post = SL + n_mask - n_pre
+            n_post = sl + n_mask - n_pre
             j = i + n_pre + 8
             s = bb[i:i+n_pre] + bb[j:j+n_post]
             print(f'{i} - {i+n_pre} ({n_pre}) + {j}:{j+n_post} ({n_post})')
             j += n_post
             need_parse_mini = 1
         else:
-            j = i + n_mask + SL
+            j = i + n_mask + sl
             s = bb[i:j]
             print(f'{i} - {j} ({j - i})')
             need_parse_mini = 0
 
+
+        # sample second
         _parse_sample(s[n_mask:], t, f_csv)
         i = j
 
     f_csv.close()
 
-    # useful during development
+
+    # useful during development, copy converted file here
     c = f'cp {path_csv} .'
     sp.run(c, shell=True)
 
@@ -197,6 +225,6 @@ def parse_file_lidxv5(p):
 
 
 if __name__ == '__main__':
-    path = '/home/kaz/Downloads/dl_bil_v2/F0-5E-CD-25-92-EA/2222222_TST_20250910_160915.lid'
-    parse_file_lidxv5(path)
+    path = '/home/kaz/Downloads/2508703_LAB_20250820_161106.lid'
+    parse_file_lid_v5(path)
     
---
+++ lix/temperature.py
@@ -0,0 +1,46 @@
+from functools import lru_cache
+from numpy import log
+
+
+ZERO_KELVIN = -273.15
+MAX_INT16 = 65535
+
+
+class Temperature:
+    def __init__(self, calibration):
+        coefficients = calibration.coefficients
+        self.tma = coefficients['TMA']
+        self.tmb = coefficients['TMB']
+        self.tmc = coefficients['TMC']
+        self.tmd = 0
+        if 'TMD' in coefficients.keys():
+            self.tmd = coefficients['TMD']
+        self.tmr = coefficients['TMR']
+
+    def convert(self, raw_temperature):
+        try:
+            temp = (raw_temperature * self.tmr) / (MAX_INT16 - raw_temperature)
+            return 1 / (self.tma +
+                        self.tmb * log(temp) + self.tmd*(log(temp))**2 +
+                        self.tmc * (log(temp)) ** 3) + ZERO_KELVIN
+        except ZeroDivisionError:
+            return ZERO_KELVIN
+
+
+
+class LixFileConverterT:
+    def __init__(self, a, b, c, d, r):
+        self.coefficients = dict()
+        self.coefficients['TMA'] = a
+        self.coefficients['TMB'] = b
+        self.coefficients['TMC'] = c
+        self.coefficients['TMD'] = d
+        self.coefficients['TMR'] = r
+        self.cnv = Temperature(self)
+
+    @lru_cache
+    def convert(self, raw_temperature):
+        # _p(f'\nLixFileConverterT coefficients {self.coefficients}')
+        # _p(f'raw T {raw_temperature} converted T {self.cnv.convert(raw_temperature)}')
+        return self.cnv.convert(raw_temperature)
+

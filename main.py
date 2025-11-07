2dbb46de255dc747d65722bae7c5e00655ee806a
kaz <kaz>
2025-11-07 13:37:03 -0500

    more

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from lix.lix import parse_lid_v2_data_file
 
 
 if __name__ == '__main__':
-    path = '/home/kaz/Downloads/dl_bil_v5/2222222_BIL_20251024_173137.lid'
+    path = '/home/kaz/Downloads/dl_bil_v5/2222222_BIL_20251107_172019.lid'
     parse_lid_v2_data_file(path)

de5a79a34e201cf2d52ba5e9fa98eddc9f1a9045
kaz <kaz>
2026-01-20 12:02:22 -0500

    more

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@
 0.1.1   09 / 10 / 25
         Started tracking versions of package lix in pyproject.toml
 
+0.1.2   01 / 20 / 26
+        Made use compensated pressure instead of raw one in CSV files
+
--- lix/lix.py
+++ lix/lix.py
@@ -377,7 +377,7 @@ def _parse_sample(bb, t, fo, lct, lcp, prc, prd, cqa, cqb, cqc):
                 f'{cpd},{vax},{vay},{vaz},{c2c1},{c1c2},{v1v2},{v2v1},' \
                 f'{ratio_cv},{conductivity_ms_cm:.3f},{teos_10:.3f}\n'
         else:
-            s = (f'{t_str},{vt},{rpd},{vax},{vay},{vaz},{c2c1},{c1c2},{v1v2},{v2v1},'
+            s = (f'{t_str},{vt},{cpd},{vax},{vay},{vaz},{c2c1},{c1c2},{v1v2},{v2v1},'
                  f'{ratio_cv},{conductivity_ms_cm:.3f},{teos_10:.3f}\n')
         fo.write(s)
 
